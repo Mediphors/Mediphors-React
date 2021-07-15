9981be437fcb6994445d460b7360473009306371
Ryan Kennedy <rskennedy99@gmail.com>
2021-07-15 13:19:37 -0700

    fix navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ class Navbar extends React.Component {
         if (this.props.type === "login") {
             return (
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                  <img src="Mediphors-React/public/mediphors-logo.png" width="30" height="30" class="d-inline-block align-top" alt=""></img>
+                  <img src={process.env.PUBLIC_URL + '/mediphors-logo.png'} width="30" height="30" class="d-inline-block align-top" alt=""></img>
                   <a className="navbar-brand" href="/">Mediphors</a>
                   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
